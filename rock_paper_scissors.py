ed402cade43623199b8506f533dfe76afc76781d
Shanlin Chen <shanlinc@stanford.edu>
2023-02-14 15:35:55 -0800

    Add user input method

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,8 +1,14 @@
+
+
 def main():
     # greet the user when it starts
     print('Greetings! \n')
+    # explain the rules
     print('Let me explain the rules to you! You will play a rock-paper-scissors game with our computer.\nBoth player can choose one from the three things, and the winner is decided base on: \nrock==>scissors, scissors==>paper, paper==>rock! \n')
-    print('now, your pick! (remember to use lower case letters)')
+    print('now, your pick! (remember to type lower-case letters): ')
+    # get the user input (assume the user will type in rock/paper/scissors (lower-case))
+    x = input()
+
 
 
 if __name__ == '__main__':

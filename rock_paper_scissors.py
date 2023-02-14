e2f28b336ad3b8d66ff88825eb38ba700948d40a
Shanlin Chen <shanlinc@stanford.edu>
2023-02-14 15:29:08 -0800

    Add the rules of rock-paper-scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,8 @@
 def main():
     # greet the user when it starts
-    print('Greetings!')
+    print('Greetings! \n')
+    print('Let me explain the rules to you! You will play a rock-paper-scissors game with our computer.\nBoth player can choose one from the three things, and the winner is decided base on: \nrock==>scissors, scissors==>paper, paper==>rock! \n')
+    print('now, your pick! (remember to use lower case letters)')
 
 
 if __name__ == '__main__':
